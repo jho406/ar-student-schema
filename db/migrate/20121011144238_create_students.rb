14d66db0123ec0ddc700d47b87f1a401d17a023d
Apprentice <apprentice@dbc08.local>
2013-03-28 13:53:52 -0700

    cleaned up student migrations

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,8 +4,6 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
-#first_name,last_name,gender,birthday,email,phone
-#Kyle,Snell,male,1970-01-01,kyle.snell@example.com,415-555-1212
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
@@ -13,9 +11,6 @@ class CreateStudents < ActiveRecord::Migration
       t.date :birthday
       t.string :email
       t.string :phone
-
-#      t.timestamps
     end
-    # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
