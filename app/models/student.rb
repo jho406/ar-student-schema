51c865cf4a40649a216873f3484a547435f58052
Apprentice <apprentice@dbc08.local>
2013-03-28 11:20:15 -0700

    finished table creation and validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 require_relative '../../db/config'
 
 # implement your Student model here
+class Student < ActiveRecord::Base 
+  validates :email, :uniqueness=> true, :format => { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/}  
+  validates :age, :numericality => {:greater_than => 5}
+  validates :raw_phone, :length => { :minimum => 10 }
+
+  def name
+    first_name + " " + last_name
+  end
+
+  def age
+    now = Date.today
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
+
+  def raw_phone
+    phone.gsub(/\D/, '')
+  end
+
+#validates :age, :numericality => true, :on => :update
+#validates :email, :uniqueness => true, :on => :create
+end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,18 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
+#first_name,last_name,gender,birthday,email,phone
+#Kyle,Snell,male,1970-01-01,kyle.snell@example.com,415-555-1212
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string  :gender
+      t.date :birthday
+      t.string :email
+      t.string :phone
+
+#      t.timestamps
+    end
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
